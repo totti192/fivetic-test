1a1eaa41d7b9a488868e9cda58601bb27fa31ce9
dhse22 <stardhse@gmail.com>
2021-10-03 20:24:10 -0500

    hola

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+export function App() {
+    return(
+        <><div className="contenedor">
+            "hola mundo desde un componente en otro archivo</div>
+        <header>
+            <h1>
+                Esta es mi tiendoa
+            </h1>
+        </header>
+    );
+}
