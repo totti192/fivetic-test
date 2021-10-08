31d4045e3d08254f16b7a23daaf37e70f725d13e
Jose Contreras <jcontreras92tj@gmail.com>
2021-10-08 02:35:48 -0500

    creacion botones de menu header

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-import './media/icons/bxs-group.svg';
+
 
 
 function App() {
@@ -7,12 +7,14 @@ function App() {
     <div>
       <header>
         <ul className="navbar">
-            <li>
-                <img src="./media/Logo.PNG" alt="imagen" className="Logo"/></li>
-            <li><img src="./media/icons/bxs-group.svg" alt="Usuario" className="usuario"/>
-            <button className="button">Usuario</button></li>
-            <li><button className="button">Productos</button></li>
-            <li><button className="button">Ventas</button></li>
+          <div>
+            <img src="./media/Logo.PNG" alt="imagen" className="Logo"/>
+          </div>
+          <div className="menu">
+              <li><img src="./media/icons/bxs-group.svg" alt="Usuario"/><button>Usuario</button></li>
+              <li><img src="./media/icons/bxs-carousel.svg" alt="Productos"/><button>Productos</button></li>
+              <li><img src="./media/icons/bxs-spreadsheet.svg" alt="Ventas" /><button>Ventas</button></li>
+          </div>
 
         </ul>
       </header>
