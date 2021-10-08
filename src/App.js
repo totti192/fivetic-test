28a797243a1f7aa35f62e819f5e68b3c1ed4742e
Jose Contreras <jcontreras92tj@gmail.com>
2021-10-08 17:18:52 -0500

    creación main y body

--- src/App.js
+++ src/App.js
@@ -1,7 +1,5 @@
 import './App.css';
 
-
-
 function App() {
   return (
     <div>
@@ -15,9 +13,69 @@ function App() {
               <li><img src="./media/producto.png" alt="Productos"/><button>PRODUCTOS</button></li>
               <li><img src="./media/ventas.png" alt="Ventas" /><button>VENTAS</button></li>
           </div>
-
         </ul>
       </header>
+     
+     
+      <main>
+      <h1 className="ventas">Ventas</h1>
+      </main>
+
+      <body>
+        <div className="paneles">
+          <div className="panelI">
+            <form>
+            <label>
+              Factura N°
+              <input placeholder="ID Venta" className="idFactuta"/>
+            </label>
+            <ul className="bloqueProductos">
+              <li># item</li>
+              <li>ID Producto</li>
+              <li>Descripción Producto</li>
+              <li>Cantidad</li>
+              <li>Precio producto unidad</li>
+              <li>Total</li>
+              <li><img src="./media/Icons/bxs-check-square.svg" alt="Confirmar"/><img src="./media/Icons/bx-trash.svg" alt="Eliminar"/></li>
+              <input type="search" zise="12" maxLength="40"/>
+              <input type="search" zise="12" maxLength="40"/>
+              <input type="search" zise="12" maxLength="40"/>
+              <input type="search" zise="12" maxLength="40"/>
+              <input type="search" zise="12" maxLength="40"/>
+              <input type="search" zise="12" maxLength="40"/>
+              <li><button><img src="./media/Icons/bxs-check-square.svg" alt="Confirmar"/></button><button><img src="./media/Icons/bx-trash.svg" alt="Eliminar"/></button></li>
+            </ul>
+            <label>
+              Vendedor
+              <input type="search"/>
+            </label>
+            <label>
+              Valor total
+              <input type="number"/>
+            </label>
+            </form>
+          </div>
+          <div className="panelD">
+            <form>
+            <label>
+              Factura N°
+              <input placeholder="ID Venta"/>
+            </label>
+            <ul className="bloqueProductos">
+            </ul>
+            <label>
+              Vendedor
+              <input type="search"/>
+            </label>
+            <label>
+              Valor total
+              <input type="number"/>
+            </label>
+            </form>
+          </div>
+        
+        </div>
+      </body>
     </div>
   );
 }
